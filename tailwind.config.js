e78e9d3aafd0e1909d9ceec36455b5a3af943f78
Alexander Lee <85447242+alexander-leeeee@users.noreply.github.com>
2026-01-19 22:44:27 +0200

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,17 @@ export default {
     "./*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class', // Это активирует класс "dark" из тега <html>
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#050505',
+        surface: '#121212',
+        primary: '#6d28d9',
+        secondary: '#2563eb',
+        accent: '#00d4ff',
+      },
+    },
   },
   plugins: [],
 }
