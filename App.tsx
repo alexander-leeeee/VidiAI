ffb171abab8354448559cb04537cb07807b57ac5
Alexander Lee <85447242+alexander-leeeee@users.noreply.github.com>
2026-01-28 18:03:35 +0200

    App.tsx

--- App.tsx
+++ App.tsx
@@ -107,7 +107,7 @@ const App: React.FC = () => {
                 onVideoGenerated={handleVideoGenerated} 
                 lang={lang} 
                 initialPrompt={templatePrompt}
-                templateId={selectedTemplateId}
+                templateId={selectedTemplate?.id}
              />
         </div>
         <div className={`transition-opacity duration-300 ${activeTab === Tab.LIBRARY ? 'opacity-100' : 'hidden absolute inset-0'}`}>
