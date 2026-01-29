475e4fd599b10d57627dd2e99b27403f7ef4623f
Alexander Lee <85447242+alexander-leeeee@users.noreply.github.com>
2026-01-29 13:35:26 +0200

    Icons.tsx

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -38,8 +38,23 @@ export const CreditCardIcon = ({ active }: { active: boolean }) => (
 );
 
 export const CoinsIcon = ({ className = "w-4 h-4 text-yellow-500" }: { className?: string }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={className}>
-    <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clipRule="evenodd" />
+  <svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    viewBox="0 0 24 24" 
+    fill="none" 
+    stroke="currentColor" 
+    strokeWidth="2" 
+    strokeLinecap="round" 
+    strokeLinejoin="round" 
+    className={className}
+  >
+    {/* Основная монета */}
+    <circle cx="8" cy="8" r="6" />
+    {/* Декоративная линия (эффект стопки) */}
+    <path d="M18.09 10.37A6 6 0 1 1 10.34 18" />
+    {/* Символ внутри или декор */}
+    <path d="M7 6v4" />
+    <path d="M17 10v4" />
   </svg>
 );
 
